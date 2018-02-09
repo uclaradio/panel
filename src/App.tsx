c730594b08d690ae51d3bcffd82bfda59ba94bf1
Nathan Smith <nathan@nathansmith.io>
2018-02-08 18:47:16 -0800

    Add main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import * as React from 'react';
 // import { Route } from 'react-router-dom';
 
-import Background from './Background/Background';
-import LoginPage from './LoginPage/LoginPage';
+// import Background from './Background/Background';
+// import LoginPage from './LoginPage/LoginPage';
 // import SignupPage from './SignupPage/Signup';
+
+import MainPage from './MainPage/MainPage';
 
 class App extends React.Component {
   render() {
     return (
-      <>
-        <Background />
-        <LoginPage />
-      </>
+      // <>
+      //   <Background />
+      //   <LoginPage />
+      // </>
+      <MainPage />
     );
   }
 }
---
+++ src/MainPage/MainPage.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+class MainPage extends React.Component {
+  render() {
+    const haejin = 'Haejin';
+
+    return <h1>Hello {haejin + 5 + 4}</h1>;
+  }
+}
+
+export default MainPage;
