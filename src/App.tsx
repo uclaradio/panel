e6923cef2fed228d231a10940727c6cdd1c0c3a1
Nathan Smith <nathan@nathansmith.io>
2018-02-09 16:45:15 -0800

    Move router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Background from './Background/Background';
 import Login from './authentication/Login/Login';
@@ -11,7 +11,7 @@ import Shows from './Shows/Shows';
 class App extends React.Component {
   render() {
     return (
-      <>
+      <Router>
         <Background />
         <Switch>
           {/* Public Routes */}
@@ -23,7 +23,7 @@ class App extends React.Component {
           <Route path="/profile" exact={true} component={Profile} />
           <Route path="/shows" exact={true} component={Shows} />
         </Switch>
-      </>
+      </Router>
     );
   }
 }
