8db95713006fe67118d4c5a28e033e8c09c38ec9
Nathan Smith <nathan@nathansmith.io>
2018-02-09 19:17:16 -0800

    Route fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import Background from './Background/Background';
 import Login from './authentication/Login/Login';
@@ -11,19 +12,21 @@ import Shows from './Shows/Shows';
 class App extends React.Component {
   render() {
     return (
-      <>
-        <Background />
-        <Switch>
-          {/* Public Routes */}
-          <Route path="/login" component={Login} />
-          <Route path="/register" component={Register} />
+      <Router>
+        <>
+          <Background />
+          <Switch>
+            {/* Public Routes */}
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
 
-          {/* Private Routes */}
-          <Route path="/" exact={true} component={Home} />
-          <Route path="/profile" exact={true} component={Profile} />
-          <Route path="/shows" exact={true} component={Shows} />
-        </Switch>
-      </>
+            {/* Private Routes */}
+            <Route path="/" exact={true} component={Home} />
+            <Route path="/profile" exact={true} component={Profile} />
+            <Route path="/shows" exact={true} component={Shows} />
+          </Switch>
+        </>
+      </Router>
     );
   }
 }
