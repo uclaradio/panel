6da2c0931f614adab100d7c294cdb6703092f39f
Nathan Smith <nathan@nathansmith.io>
2018-02-09 16:45:46 -0800

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,9 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root') as HTMLElement
-);
+ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
 registerServiceWorker();
