e35a6c4ce98fb8923aaa520005c2b4a5ef5e4acc
Daniel Guo <dguo@ucla.edu>
2018-02-15 18:40:51 -0800

    initial style changes

--- src/LoginPage/LoginForm.tsx
+++ src/LoginPage/LoginForm.tsx
@@ -3,20 +3,35 @@ import glamorous from 'glamorous';
 
 const RadioInput = glamorous.input({
   backgroundColor: 'transparent',
+  borderColor: 'black',
+  borderWidth: '0.5px',
+  borderRadius: 5,
+});
+
+const LoginButton = glamorous.button({
+  backgroundColor: '#cc00cc',
+  color: 'white',
+  border: '0px',
+  padding: '7px 15px',
+  borderRadius: 10,
 });
 
 const LoginForm = () => (
   <form>
     <label>
-      Email:
+      Email: 
       <RadioInput type="text" name="name" />
     </label>
+    <br/>
+    <br/>
     <label>
-      Password:
+      Password: 
       <RadioInput type="text" name="name" />
     </label>
-    <button type="submit">Submit</button>
+    <br/>
+    <LoginButton type="submit">Login</LoginButton>
     <span>I forgot my password :(</span>
+    <br/>
     <span>or, request an account</span>
   </form>
 );
