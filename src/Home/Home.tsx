a41c444df1ef5ba5bbce67a724e22b0dd9e09689
Haejin Jo <professionalhaejin@gmail.com>
2018-03-01 22:35:32 -0800

    First steps with css grid

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -7,40 +7,81 @@ const { Div } = glamorous;
 const CircleButton = glamorous.button({
   borderRadius: '50%',
   backgroundColor: 'black',
-  width: '5vw',
-  height: '5vw',
-  padding: 5,
+  width: '4vw',
+  height: '4vw',
+  padding: 3,
 });
 
 const RectangleThing = glamorous.div({
-  width: 100,
-  height: 50,
-  backgroundColor: 'white',
+  width: 380,
+  height: 250,
+  backgroundColor: 'rgba(255, 255, 255, 0.8)',
 });
 
+// const LinkGrid = glamorous.div({
+//   display:"grid",
+//   grid-template-columns: 400 400 400,
+//   grid-column-gap: 40px,
+
+// })
 class Home extends React.Component {
   render() {
     return (
-      <Div>
-        <Div id="nav">
+      <Div margin="5px auto" padding="20px" width="100%">
+        <Div id="nav" display="grid" grid-template-columns="80vw 20vw">
           <h1> panel.uclaradio.com: links </h1>
-          <Div className="buttonGroup" textAlign="right">
+          <Div
+            className="buttonGroup"
+            display="grid"
+            grid-template-columns="5vw 5vw 5vw"
+          >
             <CircleButton />
             <CircleButton />
             <CircleButton />
           </Div>
         </Div>
 
-        <Div id="search">
+        <Div id="search" margin-bottom="25px">
           <Div textAlign="center">
-            <input type="text" id="searchBar" />
+            <input type="text" id="searchBar" color="rgba(255,255,255,.5)" />
           </Div>
         </Div>
 
-        <Div id="link-grid">
-          <RectangleThing />
-          <RectangleThing />
-          <RectangleThing />
+        <Div
+          className="link-grid"
+          display="grid"
+          grid-template-columns="repeat(3,400px)"
+          grid-template-rows="repeat(9, 400px)"
+          grid-column-gap="50px"
+          padding="50px"
+        >
+          <RectangleThing> 1 </RectangleThing>
+          <RectangleThing> 2 </RectangleThing>
+          <RectangleThing> 3 </RectangleThing>
+          <RectangleThing> 1 </RectangleThing>
+          <RectangleThing> 2 </RectangleThing>
+          <RectangleThing> 3 </RectangleThing>
+          <RectangleThing> 1 </RectangleThing>
+          <RectangleThing> 2 </RectangleThing>
+          <RectangleThing> 3 </RectangleThing>
+          <RectangleThing> 1 </RectangleThing>
+          <RectangleThing> 2 </RectangleThing>
+          <RectangleThing> 3 </RectangleThing>
+          <RectangleThing> 1 </RectangleThing>
+          <RectangleThing> 2 </RectangleThing>
+          <RectangleThing> 3 </RectangleThing>
+          <RectangleThing> 1 </RectangleThing>
+          <RectangleThing> 2 </RectangleThing>
+          <RectangleThing> 3 </RectangleThing>
+          <RectangleThing> 1 </RectangleThing>
+          <RectangleThing> 2 </RectangleThing>
+          <RectangleThing> 3 </RectangleThing>
+          <RectangleThing> 1 </RectangleThing>
+          <RectangleThing> 2 </RectangleThing>
+          <RectangleThing> 3 </RectangleThing>
+          <RectangleThing> 1 </RectangleThing>
+          <RectangleThing> 2 </RectangleThing>
+          <RectangleThing> 3 </RectangleThing>
         </Div>
       </Div>
     );
