7ee77ca23ad7c1f53c8152a9e0379679e6e032a8
Haejin Jo <professionalhaejin@gmail.com>
2018-05-03 18:38:11 -0700

    material-ui now, still getting error with no exported member from Module

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -4,62 +4,40 @@ import './Home.css';
 const { Div } = glamorous;
 import PageCard from './PageCard';
 import CircleButton from './CircleButton';
+import Grid from 'material-ui/Grid';
 
 class Home extends React.Component {
   render() {
     return (
       <Div margin="5px auto" padding="20px" width="100%">
-        <nav>
-          <h1 id="titleText"> panel.uclaradio.com: links </h1>
-          <Div className="buttonGroup">
-            <CircleButton />
-            <CircleButton />
-            <CircleButton />
-          </Div>
-        </nav>
+        <Grid container spacing={24}>
+          <Grid item>
+            <nav>
+              <h1 id="titleText"> panel.uclaradio.com: links </h1>
+              <Div className="buttonGroup">
+                <CircleButton />
+                <CircleButton />
+                <CircleButton />
+              </Div>
+            </nav>
 
-        <Div id="search" display="flex" justifyContent="center">
-          <Div id="searchPadding" width={600} padding="30px">
-            <input placeholder="Search" type="text" id="searchBar" />
-          </Div>
-        </Div>
+            <Div id="search" display="flex" justifyContent="center">
+              <Div id="searchPadding" width={600} padding="30px">
+                <input placeholder="Search" type="text" id="searchBar" />
+              </Div>
+            </Div>
+          </Grid>
 
-        <Div
-          className="link-grid"
-          display="grid"
-          grid-template-columns="repeat(auto-fill,minmax(400px, 1fr))"
-          grid-template-rows="repeat(9, minmax(400px, 1fr))"
-          grid-column-gap="30px"
-          padding="50px"
-        >
-          <PageCard> 1 </PageCard>
-          <PageCard> 2 </PageCard>
-          <PageCard> 3 </PageCard>
-          <PageCard> 4 </PageCard>
-          <PageCard> 5 </PageCard>
-          <PageCard> 6 </PageCard>
-          <PageCard> 7 </PageCard>
-          <PageCard> 8 </PageCard>
-          <PageCard> 9 </PageCard>
-          <PageCard> 10 </PageCard>
-          <PageCard> 11 </PageCard>
-          <PageCard> 12 </PageCard>
-          <PageCard> 13 </PageCard>
-          <PageCard> 14 </PageCard>
-          <PageCard> 15 </PageCard>
-          <PageCard> 16 </PageCard>
-          <PageCard> 2 </PageCard>
-          <PageCard> 3 </PageCard>
-          <PageCard> 1 </PageCard>
-          <PageCard> 2 </PageCard>
-          <PageCard> 3 </PageCard>
-          <PageCard> 1 </PageCard>
-          <PageCard> 2 </PageCard>
-          <PageCard> 3 </PageCard>
-          <PageCard> 1 </PageCard>
-          <PageCard> 2 </PageCard>
-          <PageCard> 3 </PageCard>
-        </Div>
+          <Grid item sm={4}>
+            <PageCard />
+          </Grid>
+          <Grid item sm={4}>
+            <PageCard />
+          </Grid>
+          <Grid item sm={4}>
+            <PageCard />
+          </Grid>
+        </Grid>
       </Div>
     );
   }
